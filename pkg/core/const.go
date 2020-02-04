257cd2b6afd0dc2f76a20dc8c4a0ba8fac53c363
mkawserm <mkawserm@gmail.com>
2020-02-04 10:47:05 +0600

    VERSION UPDATE

--- pkg/core/const.go
+++ pkg/core/const.go
@@ -2,7 +2,7 @@ package core
 
 //APP RELATED CONSTANTS
 const ConstAppName = "pisig"
-const ConstAppVersion = "v1.0.0"
+const ConstAppVersion = "v0.0.0"
 const ConstAppAuthors = "MD KAWSER MUNSHI"
 const ConstAppDescription = ""
 const ConstAppNameLong = "pisig service"
