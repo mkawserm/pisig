b1bfeba9ccd74de55aa547c017fdd23d88de8e38
mkawserm <mkawserm@gmail.com>
2020-02-05 10:57:08 +0600

    PISIG SERVICE INTERFACE UPDATED

--- pkg/service/pisigservice.go
+++ pkg/service/pisigservice.go
@@ -8,8 +8,8 @@ import (
 type PisigService interface {
 	Setup(pisigContext *context.PisigContext)
 
-	SetSettings(settings map[string]interface{}) (error, bool)
-	UpdateSettings(settings map[string]interface{}) (error, bool)
+	// SetSettings(settings map[string]interface{}) (error, bool)
+	// UpdateSettings(settings map[string]interface{}) (error, bool)
 
 	GroupName() string
 	ServiceName() string
