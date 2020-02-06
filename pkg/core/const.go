8b521158e5cbb169f4f3e409fbf818ed24bde3d0
mkawserm <mkawserm@gmail.com>
2020-02-06 17:19:47 +0600

    VERSION BUMP v0.1.1

--- pkg/core/const.go
+++ pkg/core/const.go
@@ -2,7 +2,7 @@ package core
 
 //APP RELATED CONSTANTS
 const ConstAppName = "pisig"
-const ConstAppVersion = "v0.1.0"
+const ConstAppVersion = "v0.1.1"
 const ConstAppAuthors = "MD KAWSER MUNSHI"
 const ConstAppDescription = ""
 const ConstAppNameLong = "pisig service"
