c3772abccb1af9872fb4fa2e7c238e428b5daf32
mkawserm <mkawserm@gmail.com>
2020-02-08 19:40:35 +0600

    VERSION BUMP

--- pkg/core/const.go
+++ pkg/core/const.go
@@ -2,7 +2,7 @@ package core
 
 //APP RELATED CONSTANTS
 const ConstAppName = "pisig"
-const ConstAppVersion = "v0.1.3"
+const ConstAppVersion = "v0.1.4"
 const ConstAppAuthors = "MD KAWSER MUNSHI"
 const ConstAppDescription = "Pisig is a reusable high performance websocket focused web framework"
 const ConstAppNameLong = "pisig service"
