2fc640c90a01ede9c9f3bde0512fef1f9cc20be0
mkawserm <mkawserm@gmail.com>
2020-03-05 16:00:37 +0600

    VERSION UPDATED TO v0.1.9

--- pkg/core/const.go
+++ pkg/core/const.go
@@ -2,7 +2,7 @@ package core
 
 //APP RELATED CONSTANTS
 const ConstAppName = "pisig"
-const ConstAppVersion = "v0.1.8"
+const ConstAppVersion = "v0.1.9"
 const ConstAppAuthors = "MD KAWSER MUNSHI"
 const ConstAppDescription = "Pisig is a reusable high performance websocket focused web framework"
 const ConstAppNameLong = "pisig service"
